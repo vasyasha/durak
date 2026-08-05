7cedd80a5c442abaf5dcacd539cbcf09c09b791d
vasyasha <yanchos@gmail.com>
2026-08-05 17:39:42 +0100

    starting work on actual website visuals

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import GameScreen from './GameScreen.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <GameScreen />
     </StrictMode>,
 );
