406ab98976dd2d45f779bbbcf974b3a5d0588503
vasyasha <yanchos@gmail.com>
2026-08-14 17:39:53 +0100

    earaly decision to switch to phaser, will see how it plays out

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,9 @@
 import { useState, useEffect } from 'react';
-import { Texture } from 'pixi.js';
-
-import { loadGameTextures } from './utils.js';
 
 import { GameScreen } from './components/GameScreen.js';
 
 
 
 export function App() {
-    const [textures, setTextures] = useState<Record<string, Texture> | null>(null);
-
-    useEffect(() => {
-        loadGameTextures().then(setTextures);
-    }, []);
-
-
-    if (!textures) { return <div>Loading...</div>; }
     return <GameScreen />;
 }
